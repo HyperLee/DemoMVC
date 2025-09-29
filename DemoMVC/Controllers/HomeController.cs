0215c50d45b76521967ba087ca0d0431533d8ac5
Peter.Chiu <tidub76217@hotmail.com>
2025-09-30 00:05:36 +0800

    feat: 新增 Index2 頁面及其功能，包含統計資訊和互動效果

--- DemoMVC/Controllers/HomeController.cs
+++ DemoMVC/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    /// <summary>
+    /// Index2 頁面動作方法
+    /// </summary>
+    /// <returns>Index2 視圖</returns>
+    public IActionResult Index2()
+    {
+        ViewData["Title"] = "Index2 Page";
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
